abc147decc8a48a3cde12500f3dbc0e8f277eb59
Darío Poves <119043463+dapoves@users.noreply.github.com>
2024-05-22 10:22:18 +0200

    Create account.vue

---
+++ pages/account.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+     <div class="w-full h-full absolute top-0 bg-purple-700"></div>
+    <nav class="flex justify-between sticky top-0 z-10 block w-full max-w-full rounded-t-3xl px-4 py-8 bg-white h-max backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-6">
+        <NuxtLink @click="$router.go(-1)" class="cursor-pointer">
+            <IconChevronLeft class="ml-8" stroke-width="2" />
+        </NuxtLink>
+        <p class="text-2xl text-white">¡Hola Darío!</p>
+        <div class="opacity-0">
+            <IconChevronLeft class="mr-8" stroke-width="2" />
+        </div>
+    </nav>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  pageTransition: {
+    name: 'slide-left',
+    mode: 'out-in',
+    onBeforeEnter(el) {
+      console.log('Before enter...');
+    },
+    onEnter(el, done) {
+      done();
+    },
+    onAfterEnter(el) {
+      console.log('After enter');
+    }
+  }
+})
+</script>
+
+<style scoped>
+.slide-left-enter-active, .slide-left-leave-active {
+  transition: transform 0.5s ease;
+}
+
+.slide-left-enter, .slide-left-leave-to {
+  transform: translateX(100%);
+}
+
+.slide-left-leave-active {
+  position: absolute;
+}
+</style>
