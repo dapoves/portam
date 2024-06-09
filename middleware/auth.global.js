79d6b005a1a14ffe7633316fede95c4343d69180
Darío Poves <119043463+dapoves@users.noreply.github.com>
2024-06-09 20:53:07 +0200

    Added auth global middleware

---
+++ middleware/auth.global.js
@@ -0,0 +1,10 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  
+    const excludedRoutes = ['login', 'register'];
+
+  if (typeof window !== 'undefined') {
+    if (!excludedRoutes.includes(to.name) && localStorage.getItem('user_id') === null) {
+      return navigateTo('/login');
+    }
+  }
+})
--- middleware/auth.ts
+++
@@ -1,6 +0,0 @@
-import Swal from "sweetalert2"
-
-// ---cut---
-export default defineNuxtRouteMiddleware((to, from) => {
-
-})
