156c85a229cf391dfbf23667930f3d6421534ecc
Darío Poves <119043463+dapoves@users.noreply.github.com>
2024-06-12 20:11:21 +0200

    mini fix

--- pages/order.vue
+++ pages/order.vue
@@ -57,7 +57,7 @@
             <button @click="pagar"
                 class="align-middle select-none bg-purple-600 font-noto-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-4 px-6 rounded-full bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none block w-full"
                 type="button" data-ripple-light="true">
-                Pagar 24€
+                Pagar {{ precioTotal }}€
             </button>
         </div>
     </div>
