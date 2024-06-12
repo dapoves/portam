9bd6aed09fae8f28ec14b88f0af87e39ee48909e
Darío Poves <119043463+dapoves@users.noreply.github.com>
2024-06-12 20:19:40 +0200

    mini fix

--- pages/order.vue
+++ pages/order.vue
@@ -54,7 +54,7 @@
                 <p class="mb-3 font-bold text-[#434E58]">Pago Total</p>
                 <p class="font-bold text-primary">{{ precioTotal }}€</p>
             </div>
-            <button @click="pagar"
+            <button v-if="orderStore.products.length" @click="pagar"
                 class="align-middle select-none bg-purple-600 font-noto-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-4 px-6 rounded-full bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none block w-full"
                 type="button" data-ripple-light="true">
                 Pagar {{ precioTotal }}€
